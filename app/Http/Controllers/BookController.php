9dfe6d5e88d183deed9f08c84b5254566fd2633e
VDHSoft-com <139960941+VDHSoft-com@users.noreply.github.com>
2024-09-09 17:19:11 +0200

    Origin

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,66 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Book;
+use Illuminate\Http\Request;
+class BookController extends Controller
+{
+    public function index()
+    {
+        $books = Book::all();
+        
+        return view('books.index', compact('books'));
+    }
+    public function create()
+    {
+        return view('books.create');
+    }
+    public function store(Request $request)
+    {
+        /*$request->validate([
+            'title' => 'required',
+            'author' => 'required',
+            'description' => 'nullable',
+        ]);
+        
+        $data = $request->except('_token'); // Exclure le champ _token
+        Book::create($request->all());
+        
+        return redirect()->route('books.index')->with('success', 'Book created successfully.');*/
+        
+	    $validatedData = $request->validate([
+	        'title' => 'required',
+	        'author' => 'required',
+	        'description' => 'nullable',
+	    ]);
+
+	    Book::create($validatedData);
+
+	    return redirect()->route('books.index')->with('success', 'Book created successfully.');
+    }
+    public function show(Book $book)
+    {
+        return view('books.show', compact('book'));
+    }
+    public function edit(Book $book)
+    {
+        return view('books.edit', compact('book'));
+    }
+    public function update(Request $request, Book $book)
+    {
+        $request->validate([
+            'title' => 'required',
+            'author' => 'required',
+            'description' => 'nullable',
+        ]);
+        
+        $book->update($request->all());
+        
+        return redirect()->route('books.index')->with('success', 'Book updated successfully.');
+    }
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        
+        return redirect()->route('books.index')->with('success', 'Book deleted successfully.');
+    }
+}
